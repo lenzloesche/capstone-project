f5d7ca402eca07007cf0b256112d61f9f33a007c
Lenz Lösche <lenzloesche@gmail.com>
2023-03-20 16:45:11 +0100

    show search results

--- pages/strength.js
+++ pages/strength.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
 import Header from "../components/Header";
+import { uid } from "uid";
+import FormContainer from "../components/FormContainer";
 
 const apiKey = "/N+lgsT1Ci9aZ5EnpUlNFA==jE3hMgeWrU1Jd0q0";
 const url = "https://api.api-ninjas.com/v1/exercises";
 
 export default function Strength() {
   const [data, SetData] = useState([]);
-  async function fetchData() {
+  const [searchInput, setSearchInput] = useState("");
+  async function fetchData(input) {
     try {
-      const response = await fetch(url, {
+      const response = await fetch(url + input, {
         headers: {
           "x-api-key": apiKey,
         },
@@ -26,15 +29,35 @@ export default function Strength() {
   }
 
   useEffect(() => {
-    fetchData();
+    fetchData("");
   }, []);
 
   return (
     <>
       {" "}
       <Header>Fitness App</Header>
-      <input></input>
-      <p>{data[0]?.name}</p>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          fetchData("?name=" + searchInput);
+        }}
+      >
+        <input
+          value={searchInput}
+          onChange={(event) => setSearchInput(event.target.value)}
+        ></input>
+      </form>
+      {data?.map((dat) => {
+        return (
+          <FormContainer key={uid()}>
+            <p>Name: {dat?.name}</p>
+            <p>Difficulty: {dat?.difficulty}</p>
+            <p>Muslce: {dat?.muscle}</p>
+            <p>Type: {dat?.type}</p>
+            <p>Equipment: {dat?.equipment}</p>
+          </FormContainer>
+        );
+      })}
     </>
   );
 }
