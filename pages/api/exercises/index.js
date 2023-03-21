b6596b5ae00302169ec1d0ed48df71ce95d2214b
Lenz Lösche <lenzloesche@gmail.com>
2023-03-21 14:46:35 +0100

    get read from local db

---
+++ pages/api/exercises/index.js
@@ -0,0 +1,13 @@
+import dbConnect from "../../../db/connect";
+import Exercise from "../../../db/models/Exercise";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const exercises = await Exercise.find();
+    return response.status(200).json(exercises);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
+}
