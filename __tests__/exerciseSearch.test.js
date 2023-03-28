c198ee3dfd6b4f4c2d68a815fcd157e3c7d89dc8
Lenz Lösche <lenzloesche@gmail.com>
2023-03-28 13:15:15 +0200

    fixed log error in test with mock  where test tried to fetch

--- __tests__/exerciseSearch.test.js
+++ __tests__/exerciseSearch.test.js
@@ -1,5 +1,23 @@
 import ExerciseSearch from "../pages/exerciseSearch";
 import {render, screen} from "@testing-library/react";
+const todaysDate = new Date();
+
+/* const fakeData = [
+  {
+    name: "Test1",
+    difficulty: "easy",
+    muscle: "shoulders",
+    type: "strength",
+    equipment: "dumbell",
+    instructions: "TESTINSTRUCTIONS",
+  },
+];
+const userName = "Test";
+const setFavoriteExercises = () => {};
+const setFetchingStatus = () => {}; */
+const apiGetFavorite = require("../apiServices/apiGetFavorite");
+jest.mock("../apiServices/apiGetFavorite", () => () => {});
+
 test("see if search bar is rendered.", () => {
   render(<ExerciseSearch />);
   const searchName = screen.getAllByText(/search/i);
