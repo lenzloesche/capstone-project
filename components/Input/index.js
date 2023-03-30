55d66f548f83ce37d81717667f919ae076c7e3e0
Lenz Lösche <lenzloesche@gmail.com>
2023-03-30 10:50:13 +0200

    change input validation

--- components/Input/index.js
+++ components/Input/index.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const StyledInput = styled.input`
+width:200px;
   font-size: 18px;
   border: 1px solid black;
   border-radius: 8px;
@@ -15,6 +16,8 @@ export default function Input({id, type, value, onChange, required}) {
       id={id}
       type={type}
       min={type === "number" ? "0" : ""}
+      max={type === "number" ? "1440" : ""}
+      maxLength={type === "number" ? "" : "30"}
       value={value}
       onChange={onChange}
       required={required}
