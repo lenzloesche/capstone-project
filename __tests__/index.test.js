0046a80094d48f2126b19e8fe5498b1355b4ac75
Lenz Lösche <lenzloesche@gmail.com>
2023-04-11 16:21:57 +0200

    test navbar rendered

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,3 +12,13 @@ test("there is attribution on the page", () => {
   const attribution = screen.getByText(/attribution/i);
   expect(attribution).toBeInTheDocument();
 });
+
+test("navbar is renderd", () => {
+  render(<Home userName="TestName" />);
+  const dumbbellImage = screen.getByAltText(/dumbbell image/i);
+  expect(dumbbellImage).toBeInTheDocument();
+  const calendarImage = screen.getByAltText(/calendar image/i);
+  expect(calendarImage).toBeInTheDocument();
+  const foodImage = screen.getByAltText(/food image/i);
+  expect(foodImage).toBeInTheDocument();
+});
