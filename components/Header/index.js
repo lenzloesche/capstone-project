414854e897f563169c74ea3feef0537430de1ce2
Lenz Lösche <lenzloesche@gmail.com>
2023-04-13 15:56:56 +0200

    change fitness app title position for desktop

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,5 +7,9 @@ const Header = styled.header`
   grid-template-columns: 1fr 58px;
   padding-left: 14px;
   justify-content: space-between;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: center;
+  }
 `;
 export default Header;
